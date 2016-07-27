5cb67c1c89c07ee486f89e703c29f00280486f16
jaerith <Aaron.Kendall@gmail.com>
2016-07-27 16:34:07 -0400

    * Added the rest of the files needed for the initial revision of the solution.

---
+++ OnixTestHarness/ParseHelpers.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+using System.Xml;
+using System.Xml.Serialization;
+using System.Xml.Schema;
+
+namespace OnixTestHarness
+{
+    internal static class ParseHelpers
+    {
+        private static JavaScriptSerializer json;
+        private static JavaScriptSerializer JSON { get { return json ?? (json = new JavaScriptSerializer()); } }
+
+        public static Stream ToStream(this string @this)
+        {
+            var stream = new MemoryStream();
+            var writer = new StreamWriter(stream);
+            writer.Write(@this);
+            writer.Flush();
+            stream.Position = 0;
+            return stream;
+        }
+
+        public static T ParseXML<T>(this string @this) where T : class
+        {
+            XmlReaderSettings settings = new XmlReaderSettings();
+            settings.ConformanceLevel = ConformanceLevel.Document;
+            settings.ValidationType   = ValidationType.Schema;
+            settings.DtdProcessing    = DtdProcessing.Parse;
+            settings.ValidationFlags |= XmlSchemaValidationFlags.ProcessInlineSchema;
+            settings.ValidationFlags |= XmlSchemaValidationFlags.ReportValidationWarnings;
+            // settings.ValidationFlags &= ~(XmlSchemaValidationFlags.ProcessInlineSchema);
+            // settings.ValidationFlags &= ~(XmlSchemaValidationFlags.ReportValidationWarnings);
+
+            var reader = XmlReader.Create(@this.Trim().ToStream(), settings);
+            return new XmlSerializer(typeof(T), new XmlRootAttribute("ONIXMessage")).Deserialize(reader) as T;
+        }
+
+        public static T ParseJSON<T>(this string @this) where T : class
+        {
+            return JSON.Deserialize<T>(@this.Trim());
+        }
+    }
+}
---
+++ OnixTestHarness/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using OnixData.Legacy;
+
+namespace OnixTestHarness
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int    nPrdIdx = 0;
+                string xml     = File.ReadAllText(@"C:\tmp\tmp2\onix.legacy.xml");
+
+                OnixLegacyMessage onixLegacyMessage = xml.ParseXML<OnixLegacyMessage>();
+
+                foreach (OnixLegacyProduct TmpProduct in onixLegacyMessage.Product)
+                {
+                    System.Console.WriteLine("Product [" + (nPrdIdx++) + "] has EAN(" + TmpProduct.EAN + ").");
+                }
+            }
+            catch (Exception ex)
+            {
+                System.Console.WriteLine(ex);
+            }
+        }
+    }
+}
