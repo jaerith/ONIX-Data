29a76c4359a8070269b7da3a853a97a5e3a2f8ca
jaerith <Aaron.Kendall@gmail.com>
2016-12-05 11:43:05 -0500

    Added extensions class for helpful routines, especially for validating and converting product IDs.

---
+++ OnixData/Extensions/OnixDataExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using OnixData.Legacy;
+using OnixData.Version3;
+
+namespace OnixData.Extensions
+{
+    public static class OnixDataExtensions
+    {
+        #region CONSTANTS
+
+        public const int CONST_ISBN_LEN = 10;
+        public const int CONST_EAN_LEN  = 13;
+
+        #endregion
+
+    }
+}
