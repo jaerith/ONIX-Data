6df20b6c75cae5a812cb0802d2c548b4ac8380e4
jaerith <Aaron.Kendall@gmail.com>
2016-12-06 15:18:37 -0500

    Added extension to validate an EAN (i.e., a standard product id).

--- OnixData/Extensions/OnixDataExtensions.cs
+++ OnixData/Extensions/OnixDataExtensions.cs
@@ -18,5 +18,64 @@ namespace OnixData.Extensions
 
         #endregion
 
+        public static bool HasValidEAN(this OnixLegacyProduct TargetProduct)
+        {
+            bool IsValid = false;
+            string EAN = TargetProduct.EAN;
+
+            if (!String.IsNullOrEmpty(EAN))
+                IsValid = EAN.IsValidEAN();
+
+            return IsValid;
+        }
+
+        public static bool IsValidEAN(this string TargetEAN)
+        {
+            bool IsValid = false;
+
+            int[] EanWeights = { 1, 3, 1, 3, 1, 3, 1, 3, 1, 3, 1, 3, 1 };
+
+            int sum, diff;
+            int ZeroCharVal = (int)'0';
+            long value;
+
+            String TempEAN = TargetEAN;
+
+            // Initialization
+            sum = 0;
+            value = 0;
+
+            if (String.IsNullOrEmpty(TargetEAN))
+                return false;
+
+            if (TargetEAN.Length > CONST_EAN_LEN)
+                return false;
+
+            try
+            {
+                long nEAN = Convert.ToInt64(TargetEAN);
+            }
+            catch (Exception ex)
+            { return false; }
+
+            if (TargetEAN.Length < CONST_EAN_LEN)
+                TempEAN = TargetEAN.PadLeft(CONST_EAN_LEN, '0');
+
+            char[] EanCharArray = TempEAN.ToCharArray();
+            for (int i = 0; i < EanCharArray.Length; ++i)
+            {
+                int TempCharVal = (int)EanCharArray[i];
+
+                diff = (TempCharVal - ZeroCharVal);
+
+                sum += (diff) * EanWeights[i];
+
+                value = value * 10 + (diff);
+            }
+
+            IsValid = (sum % 10 == 0);
+
+            return IsValid;
+        }
     }
 }
