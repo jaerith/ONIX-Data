5499153946011646b0ce90bcaea88d3f3b1dd35a
Aaron Kendall <Aaron.Kendall@gmail.com>
2019-05-08 11:47:58 -0400

    Changed code so that repeating instances of <RightsCountry> and <RightsTerritory> are taken into account.

--- OnixData/Legacy/OnixLegacySalesRights.cs
+++ OnixData/Legacy/OnixLegacySalesRights.cs
@@ -12,7 +12,9 @@ namespace OnixData.Legacy
     {
         #region CONSTANTS
 
-        private const char CONST_LIST_DELIM = ' ';
+        public const char CONST_LIST_DELIM = ' ';
+
+        public const int  CONST_MISSING_NUM_VALUE = -1;
 
         public const int CONST_SR_TYPE_FOR_SALE_WITH_EXCL_RIGHTS    = 1;
         public const int CONST_SR_TYPE_FOR_SALE_WITH_NONEXCL_RIGHTS = 2;
@@ -23,23 +25,111 @@ namespace OnixData.Legacy
         public OnixLegacySalesRights()
         {
             salesRightsTypeField = "";
-            rightsTerritoryField = rightsCountryField = "";
-            rightsTerritoryList  = rightsCountryList  = new List<string>();
+            rightsCountryField   = shortRightsCountryField = null;
+            rightsTerritoryField = shortRightsTerritoryField = null;
+            rightsTerritoryList  = new List<string>();
+            rightsCountryList    = new List<string>();
         }
 
         private string       salesRightsTypeField;
-        private string       rightsCountryField;
+
+        private string[]     rightsCountryField;
+        private string[]     shortRightsCountryField;    
         private List<string> rightsCountryList;
-        private string       rightsTerritoryField;
+
+        private string[]     rightsTerritoryField;
+        private string[]     shortRightsTerritoryField;
         private List<string> rightsTerritoryList;
 
         #region Helpers
+
+        public string[] OnixRightsCountryField
+        {
+            get
+            {
+                string[] asOnixRightsCountryList = new string[0];
+
+                if ((this.shortRightsCountryField != null) && (this.shortRightsCountryField.Length > 0))
+                    asOnixRightsCountryList = this.shortRightsCountryField;
+                else if ((this.rightsCountryField != null) && (this.rightsCountryField.Length > 0))
+                    asOnixRightsCountryList = this.rightsCountryField;
+
+                return asOnixRightsCountryList;
+            }
+        }
+
+        public string[] OnixRightsTerritoryField
+        {
+            get
+            {
+                string[] asOnixRightsTerritoryList = new string[0];
+
+                if ((this.shortRightsTerritoryField != null) && (this.shortRightsTerritoryField.Length > 0))
+                    asOnixRightsTerritoryList = this.shortRightsTerritoryField;
+                else if ((this.rightsTerritoryField != null) && (this.rightsTerritoryField.Length > 0))
+                    asOnixRightsTerritoryList = this.rightsTerritoryField;
+
+                return asOnixRightsTerritoryList;
+            }
+        }
+
+        public List<string> RightsCountryList
+        {
+            get
+            {
+                if (this.rightsCountryList.Count() <= 0)
+                {
+                    if (this.OnixRightsCountryField.Count() > 0)
+                    {
+                        foreach (string sTmpCountryMiniList in this.OnixRightsCountryField)
+                        {
+                            List<string> TmpCountryList = new List<string>();
+
+                            if (sTmpCountryMiniList.Contains(CONST_LIST_DELIM))
+                                TmpCountryList = new List<string>(sTmpCountryMiniList.Split(CONST_LIST_DELIM));
+                            else
+                                TmpCountryList = new List<string>() { sTmpCountryMiniList };
+
+                            this.rightsCountryList.AddRange(TmpCountryList);
+                        }
+                    }
+                }
+
+                return this.rightsCountryList;
+            }
+        }
+
+        public List<string> RightsTerritoryList
+        {
+            get
+            {
+                if (this.rightsTerritoryList.Count() <= 0)
+                {
+                    if (this.OnixRightsTerritoryField.Count() > 0)
+                    {
+                        foreach (string sTmpTerritoryMiniList in this.OnixRightsTerritoryField)
+                        {
+                            List<string> TmpTerritoryList = new List<string>();
+
+                            if (sTmpTerritoryMiniList.Contains(CONST_LIST_DELIM))
+                                TmpTerritoryList = new List<string>(sTmpTerritoryMiniList.Split(CONST_LIST_DELIM));
+                            else
+                                TmpTerritoryList = new List<string>() { sTmpTerritoryMiniList };
+
+                            this.rightsTerritoryList.AddRange(TmpTerritoryList);
+                        }
+                    }
+                }
+
+                return this.rightsTerritoryList;
+            }
+        }
 
         public int SalesRightsTypeNum
         {
             get
             {
-                int nSRTypeNum = -1;
+                int nSRTypeNum = CONST_MISSING_NUM_VALUE;
 
                 if (!String.IsNullOrEmpty(this.salesRightsTypeField))
                     Int32.TryParse(this.salesRightsTypeField, out nSRTypeNum);
@@ -66,7 +156,8 @@ namespace OnixData.Legacy
         }
 
         /// <remarks/>
-        public string RightsCountry
+        [System.Xml.Serialization.XmlElementAttribute("RightsCountry")]
+        public string[] RightsCountry
         {
             get
             {
@@ -75,27 +166,12 @@ namespace OnixData.Legacy
             set
             {
                 this.rightsCountryField = value;
-
-                if (!String.IsNullOrEmpty(this.rightsCountryField))
-                {
-                    if (this.rightsCountryField.Contains(CONST_LIST_DELIM))
-                        this.rightsCountryList = new List<string>(this.rightsCountryField.Split(CONST_LIST_DELIM));
-                    else
-                        this.rightsCountryList = new List<string>() { this.rightsCountryField };
-                }
-            }
-        }
-
-        public List<string> RightsCountryList
-        {
-            get
-            {
-                return this.rightsCountryList;
             }
         }
 
         /// <remarks/>
-        public string RightsTerritory
+        [System.Xml.Serialization.XmlElementAttribute("RightsTerritory")]
+        public string[] RightsTerritory
         {
             get
             {
@@ -104,22 +180,6 @@ namespace OnixData.Legacy
             set
             {
                 this.rightsTerritoryField = value;
-
-                if (!String.IsNullOrEmpty(this.rightsTerritoryField))
-                {
-                    if (this.rightsTerritoryField.Contains(CONST_LIST_DELIM))
-                        this.rightsTerritoryList = new List<string>(this.rightsTerritoryField.Split(CONST_LIST_DELIM));
-                    else
-                        this.rightsTerritoryList = new List<string>() { this.rightsTerritoryField };
-                }
-            }
-        }
-
-        public List<string> RightsTerritoryList
-        {
-            get
-            {
-                return this.rightsTerritoryList;
             }
         }
 
@@ -140,29 +200,29 @@ namespace OnixData.Legacy
             }
         }
 
-        /// <remarks/>
-        public string b090
+        [System.Xml.Serialization.XmlElementAttribute("b090")]
+        public string[] b090
         {
             get
             {
-                return RightsCountry;
+                return shortRightsCountryField;
             }
             set
             {
-                RightsCountry = value;
+                shortRightsCountryField = value;
             }
         }
 
-        /// <remarks/>
-        public string b388
+        [System.Xml.Serialization.XmlElementAttribute("b388")]
+        public string[] b388
         {
             get
             {
-                return RightsTerritory;
+                return shortRightsTerritoryField;
             }
             set
             {
-                RightsTerritory = value;
+                shortRightsTerritoryField = value;
             }
         }
 
