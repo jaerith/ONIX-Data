df20c300a54441254f1a998ba42b50238ea24dfa
Stanislaw Szolkowski <stanislaw.szolkowski@gmail.com>
2021-07-28 13:58:41 +0200

    Add new explicit project for NET 5 unit tests and change core test project to .core 3.1. Clear up tests and fix naming.

---
+++ OnixData.Standard.Net5Tests/Version3/Text/OnixSupportingResourceTests.cs
@@ -0,0 +1,9 @@
+using OnixData.Standard.BaseTests.Version3.Text;
+
+namespace OnixData.Standard.Net5Tests.Version3.Text
+{
+    public class OnixSupportingResourceTests : BaseOnixSupportingResourceTests
+    {
+        
+    }
+}
--- OnixData.Standard.NetFrameworkTests/V3ParserTests.cs
+++ OnixData.Standard.NetFrameworkTests/V3ParserTests.cs
@@ -2,7 +2,7 @@ using OnixData.Standard.BaseTests;
 
 namespace OnixData.Standard.NetFrameworkTests
 {
-    public class OnixV3ParserTests: BaseOnixV3ParserTests
+    public class V3ParserTests: BaseOnixV3ParserTests
     {
     }
 }
